3b004bfebb7622d34d89052d339054c851ee7921
Rosa Donoso <rosa.donoso.requena@outlook.de>
2021-12-14 09:56:45 +0100

    added functions into js

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,42 @@ const playerResult = document.querySelector('.js_counts__player');
 const computerResult = document.querySelector('.js-counts__computer');
 
 
+
+//Funciones
+
+function handleClickGame() {
+    console.log("hola");
+
+}
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+const randomNumber = getRandomNumber(10) - 1;
+
+function getuserTool() {
+    const UserSelection = gameSelector.value;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Listener
+playButton.addEventListener('click', handleClickGame);
+
+
