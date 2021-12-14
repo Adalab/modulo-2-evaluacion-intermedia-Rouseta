e0ef0a31b7a08549ec2a2a0c73bc334dcd4570ab
Rosa Donoso <rosa.donoso.requena@outlook.de>
2021-12-14 09:27:58 +0100

    added HTML and const

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+//Declarar constantes globales
+const playButton = document.querySelector('.js_button');
+const gameSelector = document.querySelector('.js_button');
+const btnGameResult = document.querySelector('.btn__js__generalresult');
+const playerResult = document.querySelector('.js_counts__player');
+const computerResult = document.querySelector('.js-counts__computer');
+
+
