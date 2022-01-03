72acad11456790a31b747ca3a6267dae96335765
Rosa Donoso <rosa.donoso.requena@outlook.de>
2022-01-03 13:48:18 +0100

    fixing issues: added render function, added counters un click

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,15 @@
-'use strict';
+"use strict";
+
+// const { render } = require("node-sass");
+
 //Declarar constantes globales
-const playButton = document.querySelector('.js_button');
-const gameSelector = document.querySelector('.js_game');
-const btnGameResult = document.querySelector('.btn__js__generalresult');
-const playerResult = document.querySelector('.js_counts__player');
-const computerResult = document.querySelector('.js-counts__computer');
+const playButton = document.querySelector(".js_button");
+const gameSelector = document.querySelector(".js_game");
+const btnGameResult = document.querySelector(".btn__js__generalresult");
+const playerResult = document.querySelector(".js_counts__player");
+const computerResult = document.querySelector(".js-counts__computer");
 let userWin = 0;
 let computerWin = 0;
-
-
-
-
 
 //Function
 
@@ -18,10 +17,11 @@ let computerWin = 0;
 
 function handleClickGame(event) {
     event.preventDefault();
+    getRandomNumber();
     getuserTool();
-    compareResults()
+    compareResults();
+    counters();
     //console.log("hola");
-
 }
 
 //2. Get random number
@@ -32,57 +32,47 @@ function getRandomNumber(max) {
 const randomNumber = getRandomNumber(10) - 1;
 
 let randomTool = "";
-let user
+let user;
 
 //3. Get input value
 
 function getuserTool() {
-
     if (randomNumber > 3) {
-        randomTool = 'piedra';
+        randomTool = "piedra";
+    } else if (randomNumber >= 6) {
+        randomTool = "papel";
+    } else if (randomNumber >= 7) {
+        randomTool = "tijera";
     }
-    else if (randomNumber >= 6) {
-        randomTool = 'papel';
-
-    }
-    else if (randomNumber >= 7) {
-        randomTool = 'tijera';
-    }
-
 }
 //4.Results comparition
+function renderButton(render) {
+    btnGameResult.innerHTML = render;
+}
 
 function compareResults() {
     let userSelection = gameSelector.value;
-    console.log(userSelection, randomTool);
-
 
     if (userSelection === randomTool) {
-        btnGameResult.innerHTML = "Empate";
-    } else if (userSelection === 'piedra' && randomTool === 'tijera') {
-        btnGameResult.innerHTML = "¡Has ganado!";
+        renderButton("Empate");
+    } else if (userSelection === "piedra" && randomTool === "tijera") {
+        renderButton("¡Has ganado!");
 
         playerVictory();
-
-
-    }
-    else if (userSelection === 'tijera' && randomTool === 'papel') {
-        btnGameResult.innerHTML = "¡Has ganado!";
+    } else if (userSelection === "tijera" && randomTool === "papel") {
+        renderButton("¡Has ganado!");
         playerVictory();
-    }
-    else if (userSelection === 'papel' && randomTool === 'tijera') {
-        btnGameResult.innerHTML = "Has perdido"; computerVictory();
-
-    }
-    else if (userSelection === 'papel' && randomTool === 'piedra') {
-        btnGameResult.innerHTML = "¡Has ganado!";
+    } else if (userSelection === "papel" && randomTool === "tijera") {
+        renderButton("¡Has perdido!");
+        computerVictory();
+    } else if (userSelection === "papel" && randomTool === "piedra") {
+        renderButton("¡Has ganado!");
         playerVictory();
-    }
-    else if (userSelection === 'tijera' && randomTool === 'piedra') {
-        btnGameResult.innerHTML = "Has perdido"; computerVictory();
-    }
-    else if (userSelection === 'piedra' && randomTool === 'papel') {
-        btnGameResult.innerHTML = "Has perdido";
+    } else if (userSelection === "tijera" && randomTool === "piedra") {
+        renderButton("¡Has perdido!");
+        computerVictory();
+    } else if (userSelection === "piedra" && randomTool === "papel") {
+        renderButton("¡Has perdido!");
         computerVictory();
     }
 }
@@ -98,27 +88,11 @@ function computerVictory() {
     computerWin++;
     computerResult.innerHTML = computerWin;
 }
+function counters() {
+    playerVictory();
+    computerVictory();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 //Listener
-playButton.addEventListener('click', handleClickGame);
-
-
+playButton.addEventListener("click", handleClickGame);
